f7d92d5fe18c367032dbe695b23db64822184046
Imani <imanimuh@buffalo.edu>
2022-10-04 21:18:05 -0400

    Initial Commit

---
+++ bored nerds/ContentView.swift
@@ -0,0 +1,76 @@
+//
+//  ContentView.swift
+//  bored nerds
+//
+//  Created by Muhammad-Graham, Imani on 10/4/22.
+//
+
+import SwiftUI
+
+enum pages: Identifiable{
+    var id: Int {
+        self.hashValue
+    }
+    case page1
+    case page2
+    case page3
+}
+
+struct ContentView: View {
+    
+    @State private var activepage: pages?
+    
+    var body: some View {
+        VStack{
+            Text("Hello, world!")
+                .padding()
+            
+            Button("Open page 1"){
+                activepage = .page1
+            }
+            Button("Open page 2"){
+                activepage = .page2
+            }
+            Button("Open page 3"){
+                activepage = .page3
+            }
+            
+            
+            
+        }
+        .sheet(item: $activepage){item in
+            switch item{
+                case .page1:
+                    Text("Hello, 1!")
+                        .padding()
+                    
+                    Button("Open page 2"){
+                        activepage = .page2
+                    }
+                case .page2:
+                    Text("Hello, 2!")
+                        .padding()
+                    
+                    Button("Open page 1"){
+                        activepage = .page1
+                    }
+                case .page3:
+                    Text("Hello, 3!")
+                        .padding()
+                    
+                    Button("Open page 1"){
+                        activepage = .page1
+                    }
+                }
+                
+            }
+            
+        }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bored nerds/bored_nerdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  bored_nerdsApp.swift
+//  bored nerds
+//
+//  Created by Muhammad-Graham, Imani on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct bored_nerdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
