7e42c00dbd91c653e98e5313df9faa528761e9eb
Imani <imanimuh@buffalo.edu>
2022-10-19 01:23:28 -0400

    need to make sensor list pretty

--- bored nerds/Settings.swift
+++ bored nerds/Settings.swift
@@ -17,8 +17,9 @@ struct Settings: View {
     @State var proximity    = "Loading..."
     @State var prox:Bool = false
     
+    @State var acc_view = false
     var body: some View {
-        
+    
         //Set up Timer: for ever 1 second, update sensor values
         let _ = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
             //set up proximity updates
@@ -35,19 +36,36 @@ struct Settings: View {
             mag_data =  "\(String(sensor_list.magnetometer.show().description))"
             press_data = "\(String(sensor_list.pressure.show().description))"
         }
-        
         VStack{
-            Text("Sensor Settings")
-                .padding()
-            
-            Group {
-                Text(acc_data)
-                Text(gyro_data)
-                Text(mag_data)
-                Text(press_data)
-                Text(proximity)
-            }
-        }        
+            title(data: "Sensor Settings")
+            ScrollView( content:{
+                //Each sensor here
+                VStack{
+                    
+                    HStack{
+                        sub_title(data: "Accelerometer")
+                        Image("accelerometer_img")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .cornerRadius(20)
+                            .padding()
+                    }.onTapGesture{
+                        self.acc_view.toggle()
+                    }.background(Color.red).cornerRadius(20)
+                    
+                    Text(acc_data)
+                }.frame(height: acc_view ? 400 : 50)
+                
+                VStack{
+                    Group {
+                        Text(gyro_data)
+                        Text(mag_data)
+                        Text(press_data)
+                        Text(proximity)
+                    }
+                }
+            })
+        }
     }
     
 }
